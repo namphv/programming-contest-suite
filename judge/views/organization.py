db645879f641fc2f20ec33ae1a76ddf4154b776a
Nam Pham <namph.data@gmail.com>
2024-12-03 08:35:34 +0700

    debug

--- judge/views/organization.py
+++ judge/views/organization.py
@@ -110,23 +110,25 @@ class OrganizationUsers(QueryStringSortMixin, DiggPaginatorMixin, BaseOrganizati
     context_object_name = 'users'
 
     def get_queryset(self):
-        users = self.object.members.filter(is_unlisted=False)
-        # query_set = self.object.members.filter(is_unlisted=False).order_by(self.order) \
-        #     .select_related('user', 'display_badge').defer('about', 'user_script', 'notes')
-        # print(query_set.query)
-        # return query_set
-        if self.order in ['performance_points', '-performance_points']:
-            reverse = self.order == '-performance_points'  # Determine sorting direction
-
-            # Annotate the points for the specific organization ID
-            query_set =  users.annotate(
+        users = self.object.members.filter(is_unlisted=False).annotate(
                 org_points=Cast(
                     RawSQL("JSON_EXTRACT(organization_points, %s)", (f"$.{self.object.id}",)),
                     FloatField()
                 )
-            ).order_by('-org_points' if reverse else 'org_points').select_related('user', 'display_badge').defer('about', 'user_script', 'notes')
+            )
+        # query_set = self.object.members.filter(is_unlisted=False).order_by(self.order) \
+        #     .select_related('user', 'display_badge').defer('about', 'user_script', 'notes')
+        # print(query_set.query)
+        # return query_set
+
+        if self.order == 'performance_points':
+            query_order = 'org_points'
+        elif self.order == '-performance_points':
+            query_order = '-org_points'
         else:
-            query_set = users.order_by(self.order).select_related('user', 'display_badge').defer('about', 'user_script', 'notes')
+            query_order = self.order
+
+        query_set = users.order_by(query_order).select_related('user', 'display_badge').defer('about', 'user_script', 'notes')
         print(query_set.query)
         return query_set
 
