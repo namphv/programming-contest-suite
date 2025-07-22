83512385a2cd490449a8007c5276c62c5e761a8f
Nam Pham <namph.data@gmail.com>
2025-07-22 21:59:31 +0700

    debug

--- judge/bridge/daemon.py
+++ judge/bridge/daemon.py
@@ -19,23 +19,28 @@ def reset_judges():
 
 
 def judge_daemon():
+    logger.info('start')
     reset_judges()
+    logger.info('start ----------------------111111111111111111')
     Submission.objects.filter(status__in=Submission.IN_PROGRESS_GRADING_STATUS) \
         .update(status='IE', result='IE', error=None)
     judges = JudgeList()
+    logger.info('start -------------------------222222222222222222')
 
     judge_server = Server(settings.BRIDGED_JUDGE_ADDRESS, partial(JudgeHandler, judges=judges))
     django_server = Server(settings.BRIDGED_DJANGO_ADDRESS, partial(DjangoHandler, judges=judges))
-
+    logger.info('start ===================================')
     threading.Thread(target=django_server.serve_forever).start()
     threading.Thread(target=judge_server.serve_forever).start()
 
     stop = threading.Event()
 
+    logger.info('start =================================== 3333333333333333333333333')
     def signal_handler(signum, _):
         logger.info('Exiting due to %s', signal.Signals(signum).name)
         stop.set()
 
+    logger.info('start ==============444444444444444444')
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGQUIT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
