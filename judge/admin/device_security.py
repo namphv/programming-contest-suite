ab17f74da332ea2d9d47e07c0589b6fa009c6204
Nam Pham <namph.data@gmail.com>
2026-06-19 01:27:25 +0700

    chore: update exempt security device translation

--- judge/admin/device_security.py
+++ judge/admin/device_security.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.urls import path
 from django.utils.html import format_html
-from django.utils.translation import gettext as _, ngettext
+from django.utils.translation import gettext_lazy as _, ngettext_lazy as ngettext
 from django.conf import settings
 
 from judge.models import Profile
